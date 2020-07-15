ff1fdf63034e710a6f0d7d308df506fbbe0f82a1
EmberLynn <ember.robb@gmail.com>
2020-07-15 14:24:11 -0600

    Working on adding animation to our lonely seagull.

---
+++ MonoGameTest/AnimateSprite.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace MonoGameTest
+{
+
+    class AnimateSprite
+    {
+        public Texture2D Texture { get; set; }
+
+
+    }
+}
