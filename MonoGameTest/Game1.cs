17ee748f82af5732535870fff481530c15fb7c27
EmberLynn <ember.robb@gmail.com>
2020-07-27 18:44:48 -0600

    Added code for screen size

--- MonoGameTest/Game1.cs
+++ MonoGameTest/Game1.cs
@@ -83,7 +83,10 @@ namespace MonoGameTest
             //this contains methods for drawing sprites onto the screen
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            //for screen size; not currently using
+            //graphics.PreferredBackBufferWidth = 100;
+            //graphics.PreferredBackBufferHeight = 500; 
+            //graphics.ApplyChanges();
 
             //load our images from contents
             //refer specifically to the name of the file in contents
